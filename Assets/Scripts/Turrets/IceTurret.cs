8207a167d9dfed5c64871b14e5b90721a07e8ab4
JFK20 <clashzocker2008@gmail.com>
2023-06-01 22:11:59 +0200

    Ice Turret is now Upgradeable

--- Assets/Scripts/Turrets/IceTurret.cs
+++ Assets/Scripts/Turrets/IceTurret.cs
@@ -8,6 +8,17 @@ public class IceTurret : StandardTurret
     [SerializeField] protected float aps= 1f; //attacks per second
     [SerializeField] protected float slowingAmount = 2f;
     [SerializeField] protected float slowingTime = 2f;
+
+    private float baseAps;
+    private int levelAps = 1;
+    
+    protected override void Start() {
+        baseAps = aps;
+        baseTargetingRange = targetingRange;
+        
+        upgradeButton1.onClick.AddListener(UpgradeAps);
+        upgradeButton2.onClick.AddListener(UpgradeRange);
+    }
     
     protected override void Update() {
         timeUntilFire += Time.deltaTime;
@@ -38,6 +49,30 @@ public class IceTurret : StandardTurret
         yield return new WaitForSeconds(slowingTime);
         
         em.ResetSpeed();
+    }
+    
+    protected void UpgradeAps(){
+        if (CalculateCost(levelAps) > LevelManager.Main.currency) { return; }
+
+        LevelManager.Main.SpendCurrency(CalculateCost(levelAps));
+        levelAps++;
+        aps = CalculateBps(levelAps);
+        SoundEffectPlayer.Main.BuildandUpgradeSound();
+        if (levelAps >= 5) {
+            upgradeButton1.interactable = false;
+        }
+    }
+    
+    protected override void UpgradeRange(){
+        if (CalculateCost(levelRange) > LevelManager.Main.currency) { return; }
+
+        LevelManager.Main.SpendCurrency(CalculateCost(levelRange));
+        levelRange++;
+        targetingRange = CalculateTargetingRange(levelRange);
+        SoundEffectPlayer.Main.BuildandUpgradeSound();
+        if (levelRange >= 3) {
+            upgradeButton2.interactable = false;
+        }
     }
     
 }
